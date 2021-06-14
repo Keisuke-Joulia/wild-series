537a06c888dd7e40f28e845067dbd85ae0935f30
Keisuke Joulia <keisukejoulia@yahoo.co.jp>
2021-06-14 15:13:26 +0200

    quete 15 Doctrine relations Many-To-Many

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Vidéo de la quete 12 https://www.loom.com/share/5ac0f7b695a645c0bb5b3233d347fe2
 Vidéo de la quete 13 Part1 https://www.loom.com/share/eb800dda4fb5420090340c0bb9be6754
 Vidéo de la quete 13 Part2 https://www.loom.com/share/690cd58c91d84070881c6386587af8f2
 Vidéo de la quete 14 https://www.loom.com/share/56ba40befc7f47d7bcf5a7aa73d99e3a
+Vidéo de la quete 15 https://www.loom.com/share/c2f88b3205e14af686cfd32e87b538fc
---
+++ src/Controller/ActorController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Actor;
+use App\Form\ActorType;
+use App\Repository\ActorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/actor")
+ */
+class ActorController extends AbstractController
+{
+    /**
+     * @Route("/{id}", name="actor_show", methods={"GET"})
+     */
+    public function show(Actor $actor): Response
+    {
+        $programs = $actor->getPrograms();
+
+        return $this->render('actor/show.html.twig', [
+            'actor' => $actor,
+            'programs' => $programs,
+        ]);
+    }
+}
